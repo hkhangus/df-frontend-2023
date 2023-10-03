67e4c90f0549bcc2ccf51b4a9e5a0eceedc1a1c1
hkhanghcmus <huukhangtc@gmail.com>
2023-10-03 21:39:31 +0700

    add default book

--- assignment-4/src/app/page.tsx
+++ assignment-4/src/app/page.tsx
@@ -1,8 +1,61 @@
 'use client'
 
 import React from 'react'
+
 // import Image from 'next/image'
+import { Book, BookList } from '../types'
 import { Button } from '../components'
+
+const DEFAULT_BOOK: BookList = [
+  {
+    id: 1,
+    name: 'Refactoring',
+    author: 'Martin Fowler',
+    topic: 'Programming',
+  },
+  {
+    id: 2,
+    name: 'Design Data-Intensive Applications',
+    author: 'Martin Kleppman',
+    topic: 'Database',
+  },
+  {
+    id: 3,
+    name: 'The Phoenix Project',
+    author: 'Gene Kim',
+    topic: 'DevOps',
+  },
+  {
+    id: 4,
+    name: 'The Pragmatic Programmer: Your Journey to Mastery',
+    author: 'Andrew Hunt, David Thomas',
+    topic: 'Software Development',
+  },
+  {
+    id: 5,
+    name: 'Effective Java',
+    author: 'Joshua Bloch',
+    topic: 'Java Programming',
+  },
+  {
+    id: 6,
+    name: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    topic: 'Literature',
+  },
+  {
+    id: 7,
+    name: '1984',
+    author: 'George Orwell',
+    topic: 'Science Fiction',
+  },
+  {
+    id: 8,
+    name: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    topic: 'Literature',
+  },
+]
 
 export default function Home() {
   return (
@@ -44,7 +97,10 @@ export default function Home() {
                 <td>Refactoring</td>
                 <td>Martin Fowler</td>
                 <td>Programming</td>
-                <td className="delete-click">Delete</td>
+                <td className="delete-click">
+                  <button className=' text-red-500 border-r-2 border-solid border-red-500 pr-2'>Delete</button>
+                  <button className='text-red-500 pl-2'>View</button>
+                </td>
               </tr>
             </tbody>
           </table>

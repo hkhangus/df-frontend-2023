07747d2f0c122b62bf58446d9b7f88e2dc570f94
hkhanghcmus <huukhangtc@gmail.com>
2023-10-05 00:58:12 +0700

    Complete delete componenet

--- assignment-4/src/components/ModalDelete.tsx
+++ assignment-4/src/components/ModalDelete.tsx
@@ -1,27 +1,37 @@
 import { Dispatch, SetStateAction } from 'react'
 import { Book } from '../types'
 
-interface ModalDeleteProps{
-  book: Book,
-  openModalDelete: boolean,
-  setModalDeleteOpen: Dispatch<SetStateAction<boolean>>,
-  handleDelete: (item: Book)=>void,
+interface ModalDeleteProps {
+  book: Book
+  openModalDelete: boolean
+  setModalDeleteOpen: Dispatch<SetStateAction<boolean>>
+  handleDelete: (item: Book) => void
 }
 
+export default function ModalDelete({
+  book,
+  openModalDelete,
+  setModalDeleteOpen,
+  handleDelete,
+}: ModalDeleteProps) {
+  function closeModal() {
+    setModalDeleteOpen(false)
+  }
 
-export default function ModalDelete({ book, openModalDelete, setModalDeleteOpen, handleDelete }: ModalDeleteProps) {
-    function closeModal() {
-      setModalDeleteOpen(false)
-    }  
-    
-    function clickDelete(e,handleDelete: Function,setModalDelete:Dispatch<SetStateAction<boolean>>,book: Book){
-        e.preventDefault()
-        handleDelete(book)
-        setModalDelete(false)
-    }
-    
-    return openModalDelete ? (
-      <div className="modal modal-delete">
+  function clickDelete(
+    e,
+    handleDelete: Function,
+    setModalDelete: Dispatch<SetStateAction<boolean>>,
+    book: Book,
+  ) {
+    e.preventDefault()
+    handleDelete(book)
+    setModalDelete(false)
+  }
+
+  return openModalDelete ? (
+    <div className="modal modal-delete">
+      <div className="form-wrapper">
         <div className="form delete-form">
           <h2 className="form-header">
             Delete book
@@ -29,16 +39,26 @@ export default function ModalDelete({ book, openModalDelete, setModalDeleteOpen,
               &#x2716;
             </button>
           </h2>
-          <div className="form__content delete-item">Do you want to delete {book.name} book?</div>
-          <div className="form__btn">
-            <button className="btn-delete" onClickCapture={(e) => clickDelete(e,handleDelete,setModalDeleteOpen,book)}>Delete</button>
-            <button className="btn btn-cancel" onClick={closeModal}>
+          <div className="form__content delete-item w-full text-center">
+            Do you want to delete 
+            <span className=' text-red-500'> {book.name} </span>
+             book?
+          </div>
+          <div className="form__btn flex justify-between w-1/4">
+            <button
+              className="btn-delete"
+              onClickCapture={(e) =>
+                clickDelete(e, handleDelete, setModalDeleteOpen, book)
+              }
+            >
+              Delete
+            </button>
+            <button className="btn btn-cancel btn-primary" onClick={closeModal}>
               Cancel
             </button>
           </div>
         </div>
       </div>
-    ) : null
-      
-    
-  }
+    </div>
+  ) : null
+}
